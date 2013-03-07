d9eabb7a24667e65872fee4beb12ff222f87a862
Daniel Bretoi <daniel@bretoi.com>
2013-03-07 12:01:30 -0800

    add extra test for checking errors

--- test/mailer.spec.js
+++ test/mailer.spec.js
@@ -10,7 +10,7 @@ var app = require('../example/app'),
     should = require('should'),
     request = require('superagent'),
     Mailbox = require('test-mailbox'),
-    fs = require('fs'),
+    fs      = require('fs'),
     config = require('../example/config');
 
 /**
@@ -47,7 +47,7 @@ describe('Mailer', function () {
         done();
       });
     });
-  })
+  });
 
   describe('POST /send-mail-via-app', function () {
 
@@ -99,38 +99,33 @@ describe('Mailer', function () {
 
   });
 
-});
 
-describe('Mailer render', function() {
-  var app;
-  before(function (done) {
-    app = require("express")();
-    require('jade');
-    app.set('views', __dirname + '/../example/views');
-    app.set('view engine', 'jade');
-    app.locals.testVariable = 'Test Variable';
-    app.locals.testFunction = function () {
-      return 'Test Function';
-    };
-    var mailer = require("../index");
-    mailer.extend(app, {
-      from: "testmail@blah.com",
-      host: "localhost",
-      transportMethod: 'sendmail'
+  describe('Rendering mail', function () {
+    it('should not render an email which is missing variables', function(done) {
+      app.locals.pretty = true;
+      delete app.locals.testVariable;
+      app.renderEmail('email', {
+        to: 'test@example.com',
+        subject: 'Test Email' },
+        function (err, renderedMail) {
+          err.toString().should.match(/testVariable is not defined/);
+          app.locals.testVariable = 'Test Variable';
+          done();
+        });
     });
-    done();
+
+    it('should render an email', function(done) {
+      app.locals.pretty = true;
+      app.renderEmail('email', {
+        to: 'test@example.com',
+        subject: 'Test Email' },
+        function (err, renderedMail) {
+          var contents = fs.readFileSync('test/rendered_mail.html'); // reading file leaves trailing \n bug in node?
+          renderedMail.should.equal(contents.toString());
+          done();
+        });
+    });
 
   });
 
-  it('Should render an email', function(done) {
-    app.locals.pretty = true
-    app.renderEmail('email', {
-      to: 'test@example.com',
-      subject: 'Test Email' },
-      function (err, renderedMail) {
-        var contents = fs.readFileSync('test/rendered_mail.html') // reading file leaves trailing \n bug in node?
-        renderedMail.should.equal(contents.toString())
-        done();
-      })
-  });
 });
